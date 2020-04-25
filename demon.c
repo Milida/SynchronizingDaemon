2e28842ee2e3ab2bb452ef4effb1871acc21ed3f
milida <i.milewska@gmail.com>
2020-04-25 16:59:49 +0200

    wylistwowanie katalogu i rozpoznanie plików

--- demon.c
+++ demon.c
@@ -8,19 +8,18 @@
 #include <syslog.h>
 #include <string.h>
 #include <dirent.h>
+#include <string.h>
 
 int isDirectoryExists(const char *path){ //codeforwin.org
     struct stat stats;
     stat(path, &stats);
-
     // Check for file existence
-    return S_ISDIR(stats.st_mode);
+    return (S_ISDIR(stats.st_mode));
 }
 
 int isFileExists(const char *path){
     struct stat stats;
     stat(path, &stats);
-
     return S_ISREG(stats.st_mode);
 }
 
@@ -33,12 +32,10 @@ int main(int argc, char *argv[]) {
         printf("To many arguments\n");
         return EXIT_FAILURE;
     }
-
     printf("Source: %s\n", argv[1]);
     printf("Destination: %s\n", argv[2]);
     char *source = argv[1];
     char *destination = argv[2];
-
     if (isDirectoryExists(source)){
         printf("Directory exists at path '%s'\n", source);
         if(isDirectoryExists(destination)) {
@@ -52,70 +49,64 @@ int main(int argc, char *argv[]) {
     else {
         printf("Directory does not exist at path '%s'\n", source);
     }
-
     DIR *dp; //https://www.gnu.org/software/libc/manual/html_node/Simple-Directory-Lister.html#Simple-Directory-Lister
     struct dirent *ep;
-
     dp = opendir (source);
-    if (dp != NULL)
-    {
-        while (ep = readdir (dp))
-            printf(ep->d_name);
-            if(isFileExists(source + '/' + ep->d_name)){
-                puts(" True");
+    char *tmp;
+    char *name;
+    //strcpy(name, source);
+    //strcat(name,"/");
+    //puts(name);
+    if (dp != NULL){
+        while (ep = readdir (dp)){
+            puts(ep->d_name);
+            strcpy(name, source);
+            strcat(name,"/");
+            printf("%s%s\n",name,ep->d_name);
+            if(isFileExists(strcat(name,ep->d_name))){
+                puts("True");
             }
+        }
         (void) closedir (dp);
     }
-    else
+    else{
         perror ("Couldn't open the directory");
-
-
+    }
     /* Our process ID and Session ID */
     pid_t pid, sid;
-
     /* Fork off the parent process */
     pid = fork();
     if (pid < 0) {
         exit(EXIT_FAILURE);
     }
     /* If we got a good PID, then
-       we can exit the parent process. */
+    we can exit the parent process. */
     if (pid > 0) {
         exit(EXIT_SUCCESS);
     }
-
     /* Change the file mode mask */
     umask(0);
-
     /* Open any logs here */
-
     /* Create a new SID for the child process */
     sid = setsid();
     if (sid < 0) {
         /* Log the failure */
         exit(EXIT_FAILURE);
     }
-
-
-
     /* Change the current working directory */
     if ((chdir("/")) < 0) {
         /* Log the failure */
         exit(EXIT_FAILURE);
     }
-
     /* Close out the standard file descriptors */
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
-
     /* Daemon-specific initialization goes here */
-
     /* The Big Loop */
     //while (1) {
-        /* Do some task here ... */
-
-        //sleep(); /* wait 30 seconds */
+    /* Do some task here ... */
+    //sleep(); /* wait 30 seconds */
     //}
     exit(EXIT_SUCCESS);
 }
