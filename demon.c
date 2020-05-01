678c88dac50dedc815c21e7702fe71a3a8f7101d
dell <natalia.tarasiuk835@gmail.com>
2020-05-01 12:25:06 +0200

    all syslogs ++

--- demon.c
+++ demon.c
@@ -16,145 +16,14 @@
 #include <stdbool.h>
 #define BUFF_SIZE 64
 
-typedef struct example {
+typedef struct al {
     char *name;
-} MyExample;
+} allocation;
 
 /*
 ** http://www.unixguide.net/unix/programming/2.5.shtml
 ** About locking mechanism...
 */
-
-mode_t read_chmod(char *source){
-    struct stat mode;
-    if(stat(source, &mode) != -1){
-        return mode.st_mode;
-    }
-    else {
-        syslog(LOG_ERR,"Couldn't take a file chmod");
-        exit(EXIT_FAILURE);
-    }
-}
-
-time_t read_time(char *source){
-    struct stat time;
-    if(stat(source,&time) != -1){
-        return time.st_mtime;
-    }
-    else{
-        syslog(LOG_ERR,"Couldn't take a modification time");
-        exit(EXIT_FAILURE);
-    }
-}
-
-off_t read_size(char *source){
-    struct stat size;
-    if(stat(source, &size) != -1){
-        return size.st_size;
-    }
-    else{
-        syslog(LOG_ERR,"Couldn't take a file size");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void copy_File(char *sourceFile, char *destinationFile) {
-    char bufor[4096];
-    int readSource, writeDes;
-    int source = open(sourceFile, O_RDONLY);
-    int destination = open(destinationFile, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0644);
-    if(destination < 0 && errno == EEXIST){
-        printf("Plik %s już istnieje\n", sourceFile);
-        if(read_time(sourceFile) == read_time(destinationFile)) return;
-    }
-    destination = open(destinationFile, O_WRONLY | O_TRUNC, 0644);
-    if ((source < 0 || destination < 0) && errno != EEXIST) {
-        printf("Couldn't open the file");
-        syslog(LOG_ERR,"Couldn't open the file");
-        exit(EXIT_FAILURE);
-    }
-    while ((readSource = read(source, bufor, sizeof(bufor))) > 0){
-        writeDes = write(destination, bufor, (ssize_t) readSource);
-    }
-    syslog(LOG_INFO,"File copied successfully: %s", destinationFile);
-    printf("Copying %s file\n",sourceFile);
-
-    close(source);
-    close(destination);
-
-    mode_t source_chmod = read_chmod(sourceFile);
-    if(chmod(destinationFile, source_chmod)){
-        syslog(LOG_ERR,"Couldn't change the chmod");
-        exit(EXIT_FAILURE);
-    }
-    struct utimbuf source_time;
-    source_time.modtime = read_time(sourceFile);
-    source_time.actime = time(NULL);
-    if(utime(destinationFile, &source_time)){
-        syslog(LOG_ERR,"Couldn't change the modification time");
-        exit(EXIT_FAILURE);
-    }
-}
-
-
-void copyFile(char *sourceFile, char *destinationFile) {
-    struct stat stbuf;
-    int source = open(sourceFile, O_RDONLY);
-    int destination = open(destinationFile, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0644);
-    if(destination < 0 && errno == EEXIST){
-        printf("Plik %s już istnieje\n", sourceFile);
-        if(read_time(sourceFile) == read_time(destinationFile)) return;
-    }
-    destination = open(destinationFile, O_WRONLY | O_TRUNC, 0644);
-    if ((source < 0 || destination < 0) && errno != EEXIST) {
-        printf("Couldn't open the file");
-        syslog(LOG_ERR,"Couldn't open the file");
-        exit(EXIT_FAILURE);
-    }
-    printf("Copying %s file\n",sourceFile);
-    if(fstat(source, &stbuf)==-1){
-        printf("Fstat errno %d\n",errno);
-        syslog(LOG_ERR,"Couldn't copy the file");
-    }
-    if(sendfile(destination, source, 0, stbuf.st_size)==-1){
-        printf("%d\n",errno);
-        printf("Couldn't copy the file\n");
-        syslog(LOG_ERR,"Couldn't copy the file");
-    }
-    syslog(LOG_INFO,"File copied successfully: %s", destinationFile);
-
-    close(source);
-    close(destination);
-
-    mode_t source_chmod = read_chmod(sourceFile);
-    if(chmod(destinationFile, source_chmod)){
-        syslog(LOG_ERR,"Couldn't change the chmod");
-        exit(EXIT_FAILURE);
-    }
-
-    struct utimbuf source_time;
-    source_time.modtime = read_time(sourceFile);
-    source_time.actime = time(NULL);
-    if(utime(destinationFile, &source_time)){
-        syslog(LOG_ERR,"Couldn't change the modification time");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void deleteFile(char *destinationFile, char *sourceFile) {
-    int destination = open(destinationFile, O_RDONLY);
-    int source = open(sourceFile, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0644);
-    if (source < 0 && errno == EEXIST) {
-        printf("Plik %s już istnieje  %d\n", sourceFile, errno);
-        return;
-    } else if (remove(destinationFile) == 0)
-        printf("\nDeleted successfully %s\n", destinationFile);
-    syslog(LOG_INFO,"File deleted successfully");
-    remove(sourceFile);
-
-    close(source);
-    close(destination);
-}
 
 void handler(int signum){
     syslog(LOG_INFO,"Waking a deamon with a signal");
@@ -247,26 +116,22 @@ int main(int argc, char *argv[]){
     sid = setsid();
     if (sid < 0) {
         syslog(LOG_ERR, "Incorrect SessionID");
-        /* Log the failure */
         exit(EXIT_FAILURE);
     }
     /* Change the current working directory */
     if ((chdir("/")) < 0) {
         syslog(LOG_ERR, "Couldn't change the current working directory");
-        /* Log the failure */
         exit(EXIT_FAILURE);
     }
-
     /* Daemon-specific initialization goes here */
     signal(SIGUSR1, handler);
-    /* The Big Loop */
     while (1) {
         DIR *sourceDir; //https://www.gnu.org/software/libc/manual/html_node/Simple-Directory-Lister.html#Simple-Directory-Lister
         struct dirent *ep;
         sourceDir = opendir(source);
-        MyExample *name = (MyExample *) malloc(sizeof(MyExample));
+        Myal *name = (allocation *) malloc(sizeof(allocation));
         name->name = (char *) malloc(BUFF_SIZE);
-        MyExample *des = (MyExample *) malloc(sizeof(MyExample));
+        allocation *des = (allocation *) malloc(sizeof(allocation));
         des->name = (char *) malloc(BUFF_SIZE);
         //strcpy(name, source);
         //strcat(name,"/");
@@ -291,9 +156,9 @@ int main(int argc, char *argv[]){
         DIR *desDir;
         struct dirent *epp;
         desDir = opendir(destination);
-        MyExample *na = (MyExample *) malloc(sizeof(MyExample));
+        allocation *na = (allocation *) malloc(sizeof(allocation));
         na->name = (char *) malloc(BUFF_SIZE);
-        MyExample *desti = (MyExample *) malloc(sizeof(MyExample));
+        allocation *desti = (allocation *) malloc(sizeof(allocation));
         desti->name = (char *) malloc(BUFF_SIZE);
 
         if (desDir != NULL) {
--- filelib.c
+++ filelib.c
@@ -5,9 +5,8 @@
 
 int isDirectoryExists(const char *path){ //codeforwin.org
     struct stat stats;
-    if(stat(path, &stats) != -1){
+    if(stat(path, &stats) != -1)
         return S_ISDIR(stats.st_mode);
-    }
     return 0;
 }
 
@@ -18,4 +17,131 @@ int isFileExists(const char *path){
         return S_ISREG(stats.st_mode);
     }
     return 0;
+}
+mode_t read_chmod(char *source){
+    struct stat mode;
+    if(stat(source, &mode) != -1)
+        return mode.st_mode;
+    else {
+        syslog(LOG_ERR,"Couldn't take a file chmod");
+        exit(EXIT_FAILURE);
+    }
+}
+
+time_t read_time(char *source){
+    struct stat time;
+    if(stat(source,&time) != -1)
+        return time.st_mtime;
+    else{
+        syslog(LOG_ERR,"Couldn't take a modification time");
+        exit(EXIT_FAILURE);
+    }
+}
+
+off_t read_size(char *source){
+    struct stat size;
+    if(stat(source, &size) != -1)
+        return size.st_size;
+    else{
+        syslog(LOG_ERR,"Couldn't take a file size");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void copy_File(char *sourceFile, char *destinationFile) {
+    char bufor[4096];
+    int readSource, writeDes;
+    int source = open(sourceFile, O_RDONLY);
+    int destination = open(destinationFile, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0644);
+    if(destination < 0 && errno == EEXIST){
+        //("Plik %s już istnieje\n", sourceFile);
+        if(read_time(sourceFile) == read_time(destinationFile)) return;
+    }
+    destination = open(destinationFile, O_WRONLY | O_TRUNC, 0644);
+    if ((source < 0 || destination < 0) && errno != EEXIST) {
+        printf("Couldn't open the file");
+        syslog(LOG_ERR,"Couldn't open the file");
+        exit(EXIT_FAILURE);
+    }
+    while ((readSource = read(source, bufor, sizeof(bufor))) > 0){
+        writeDes = write(destination, bufor, (ssize_t) readSource);
+    }
+    syslog(LOG_INFO,"File copied successfully: %s", destinationFile);
+   // printf("Copying %s file\n",sourceFile);
+
+    close(source);
+    close(destination);
+
+    mode_t source_chmod = read_chmod(sourceFile);
+    if(chmod(destinationFile, source_chmod)){
+        syslog(LOG_ERR,"Couldn't change the chmod");
+        exit(EXIT_FAILURE);
+    }
+    struct utimbuf source_time;
+    source_time.modtime = read_time(sourceFile);
+    source_time.actime = time(NULL);
+    if(utime(destinationFile, &source_time)){
+        syslog(LOG_ERR,"Couldn't change the modification time");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void copyFile(char *sourceFile, char *destinationFile) {
+    struct stat stbuf;
+    int source = open(sourceFile, O_RDONLY);
+    int destination = open(destinationFile, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0644);
+    if(destination < 0 && errno == EEXIST){
+        //printf("Plik %s już istnieje\n", sourceFile);
+        if(read_time(sourceFile) == read_time(destinationFile)) return;
+    }
+    destination = open(destinationFile, O_WRONLY | O_TRUNC, 0644);
+    if ((source < 0 || destination < 0) && errno != EEXIST) {
+       // printf("Couldn't open the file");
+        syslog(LOG_ERR,"Couldn't open the file");
+        exit(EXIT_FAILURE);
+    }
+    //printf("Copying %s file\n",sourceFile);
+    if(fstat(source, &stbuf)==-1){
+       // printf("Fstat errno %d\n",errno);
+        syslog(LOG_ERR,"Couldn't copy the file");
+    }
+    if(sendfile(destination, source, 0, stbuf.st_size)==-1){
+        //printf("%d\n",errno);
+       // printf("Couldn't copy the file\n");
+        syslog(LOG_ERR,"Couldn't copy the file");
+    }
+    syslog(LOG_INFO,"File copied successfully: %s", destinationFile);
+
+    close(source);
+    close(destination);
+
+    mode_t source_chmod = read_chmod(sourceFile);
+    if(chmod(destinationFile, source_chmod)){
+        syslog(LOG_ERR,"Couldn't change the chmod");
+        exit(EXIT_FAILURE);
+    }
+
+    struct utimbuf source_time;
+    source_time.modtime = read_time(sourceFile);
+    source_time.actime = time(NULL);
+    if(utime(destinationFile, &source_time)){
+        syslog(LOG_ERR,"Couldn't change the modification time");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void deleteFile(char *destinationFile, char *sourceFile) {
+    int destination = open(destinationFile, O_RDONLY);
+    int source = open(sourceFile, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0644);
+    if (source < 0 && errno == EEXIST) {
+        printf("Plik %s już istnieje  %d\n", sourceFile, errno);
+        return;
+    } else if (remove(destinationFile) == 0){
+        printf("\nDeleted successfully %s\n", destinationFile);
+        syslog(LOG_INFO,"File deleted successfully");
+    }
+    remove(sourceFile);
+
+    close(source);
+    close(destination);
 }
