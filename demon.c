5d06fe4bd317ec1620335c0b2ff67c6571819d3c
dell <natalia.tarasiuk835@gmail.com>
2020-05-03 22:09:27 +0200

    usuniete komentarze

--- demon.c
+++ demon.c
@@ -49,36 +49,28 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    /* Our process ID and Session ID */
     pid_t pid, sid;
-    /* Fork off the parent process */
     pid = fork();
     if (pid < 0) {
         syslog(LOG_ERR, "Incorrect child pid");
         exit(EXIT_FAILURE);
     }
-    /* If we got a good PID, then
-    we can exit the parent process. */
+
     if (pid > 0) {
         syslog(LOG_INFO, "Correct child pid");
         exit(EXIT_SUCCESS);
     }
-    /* Change the file mode mask */
     umask(0);
-    /* Open any logs here */
-    /* Create a new SID for the child process */
     sid = setsid();
     if (sid < 0) {
         syslog(LOG_ERR, "Incorrect SessionID");
         exit(EXIT_FAILURE);
     }
-    /* Change the current working directory */
     if ((chdir("/")) < 0) {
         syslog(LOG_ERR, "Couldn't change the current working directory");
         exit(EXIT_FAILURE);
     }
 
-    /* Daemon-specific initialization goes here */
     signal(SIGUSR1, handler);
     while (1) {
         demonCp(source, destination, recursive, fileSize);
